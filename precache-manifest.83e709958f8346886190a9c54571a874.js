5d1f8b7c332d02a127fe4cca0450c606173041b1
Barklim <46284548+Barklim@users.noreply.github.c>
2023-08-11 17:38:19 +0800

    Updates

---
+++ precache-manifest.83e709958f8346886190a9c54571a874.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b50a61294eca8f447d8",
+    "url": "/whiteBoardCanvas/static/css/main.d01e4757.chunk.css"
+  },
+  {
+    "revision": "4b50a61294eca8f447d8",
+    "url": "/whiteBoardCanvas/static/js/main.4b50a612.chunk.js"
+  },
+  {
+    "revision": "d58b9387c1ec6fa9c2d8",
+    "url": "/whiteBoardCanvas/static/js/1.d58b9387.chunk.js"
+  },
+  {
+    "revision": "8277d40730564e5e879b",
+    "url": "/whiteBoardCanvas/static/js/runtime~main.8277d407.js"
+  },
+  {
+    "revision": "00b936c083d381360533e07e59c7f94e",
+    "url": "/whiteBoardCanvas/index.html"
+  }
+];
